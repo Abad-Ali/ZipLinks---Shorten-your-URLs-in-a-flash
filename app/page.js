79d8adf2cf37d9d2e44c52dae050c031296f722c
Abad Ali <abad02597@gmail.com>
2025-10-04 00:06:21 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
           <p className="px-20 font-serif text-lg text-center">Say goodbye to long, cumbersome URLs and hello to sleek, concise links with ZipLinks! Whether you’re sharing on social media, sending emails, or embedding in websites, ZipLinks makes it easy to create custom shortened URLs that are both user-friendly and trackable</p>
           <div className='flex gap-5 justify-start text-white'>
             <Link href="/shorten"><button className='bg-purple-700 p-4 py-2.5 font-bold rounded-xl hover:cursor-pointer'>Try Now</button></Link>
-            <Link href="/github"><button className='bg-purple-700 p-4 py-2.5 font-bold rounded-xl hover:cursor-pointer'>GitHub</button></Link>
+            <Link href="https://github.com/Abad-Ali/ZipLinks---Shorten-your-URLs-in-a-flash" target="_blank"><button className='bg-purple-700 p-4 py-2.5 font-bold rounded-xl hover:cursor-pointer'>GitHub</button></Link>
           </div>
         </div>
         <div className="flex justify-start relative md:h-[91vh]">
